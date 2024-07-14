84118ef8b54cbfbbd0e0f55ee0af44bbd4a96482
samuelmendespy <75790706+samuelmendespy@users.noreply.github.com>
2024-07-14 15:29:54 -0300

    chore: update script

--- primeiro-godot/Sprite2D.gd
+++ primeiro-godot/Sprite2D.gd
@@ -5,9 +5,7 @@ extends Sprite2D
 func _ready():
 	print("Hello World from Player")
 
-
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	var input = Input.get_vector("ui_left", "ui_right", "ui_down", "ui_up")
+	var input = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
 	position += input * speed
-	pass
