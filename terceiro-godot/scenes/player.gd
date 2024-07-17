d810919c8698ea7f5c5a7a58dfffd1822d73b7b5
samuelmendespy <75790706+samuelmendespy@users.noreply.github.com>
2024-07-17 04:45:14 -0300

    feat: add floating controls

--- terceiro-godot/scenes/player.gd
+++ terceiro-godot/scenes/player.gd
@@ -1,28 +1,19 @@
 extends CharacterBody2D
 
 
-const SPEED = 300.0
-const JUMP_VELOCITY = -400.0
+@export var speed = 4.0
+@export_range(0, 1) var lerp_factor = 0.5
+@onready var sprite = %Sprite
 
-# Get the gravity from the project settings to be synced with RigidBody nodes.
-var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
-
-
-func _physics_process(delta):
-	# Add the gravity.
-	if not is_on_floor():
-		velocity.y += gravity * delta
-
-	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction = Input.get_axis("ui_left", "ui_right")
-	if direction:
-		velocity.x = direction * SPEED
-	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
+func _physics_process(delta):
+	var direction = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
+	var target_velocity = direction * speed * 100
+	velocity = lerp(velocity, target_velocity, 0.5)
 
 	move_and_slide()
+	
+	var target_rotation = direction.x * 50
+	sprite.rotation_degrees = lerp(sprite.rotation_degrees, target_rotation, lerp_factor)
