feec92bd20a32333c73e23f365641a066e2b8a08
samuelmendespy <75790706+samuelmendespy@users.noreply.github.com>
2024-07-18 03:21:14 -0300

    feat: add invert mobs sense
    Add resources to invert mobs sense based on player position

--- sample-ts/characters/~enemies/behaviors/follow_player.gd
+++ sample-ts/characters/~enemies/behaviors/follow_player.gd
@@ -2,6 +2,8 @@ extends CharacterBody2D
 
 @export var speed: float = 1.00
 
+@onready var sprite_cpu = $AnimatedSprite2D
+
 
 func _physics_process(delta):
 	
@@ -12,3 +14,10 @@ func _physics_process(delta):
 	var input_vector = difference.normalized()
 	velocity = input_vector * speed * 100.0
 	move_and_slide()
+
+# Game logic to rotate sprite, change sprite sense
+	if input_vector.x > 0:
+		sprite_cpu.flip_h = false # desmarcar flip_h do Sprited2D
+	elif input_vector.x < 0:
+		sprite_cpu.flip_h = true # marcar flip_h do Sprite2D
+	
